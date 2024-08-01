c00126f47390f4936f2b3e1be30df5dec69595d2
Joel Cabral <108500847+JoelFedericoCabral@users.noreply.github.com>
2024-08-01 00:42:01 -0300

    mas mejoras

--- gestion/context_processors.py
+++ gestion/context_processors.py
@@ -1,4 +1,5 @@
 from gestion.models import Marca, Categoria
+from django.core.cache import cache
 
 def user_info(request):
     user = request.user if request.user.is_authenticated else None
@@ -7,10 +8,11 @@ def user_info(request):
     }
 
 def all_brands(request):
-    brands = Marca.objects.all().values_list('nombre', flat=True)
-    return {
-        'all_brands': brands
-    }
+    brands = cache.get('brands')
+    if brands is None:
+        brands = Marca.objects.all()
+        cache.set('brands', brands, 36000) 
+    return {'all_brands': brands}
 
 def all_categories(request):
     categories = Categoria.objects.all().values_list('nombre', flat=True)
