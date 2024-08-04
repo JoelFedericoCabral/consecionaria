67c4b2fd959bf3e29a03a55d4e4b19b3584c1fda
Joel Cabral <108500847+JoelFedericoCabral@users.noreply.github.com>
2024-08-04 11:56:33 -0300

    documentacion de las views mas importantes

--- gestion/views/auto_view.py
+++ gestion/views/auto_view.py
@@ -9,12 +9,23 @@ from django.utils.decorators import method_decorator
 from gestion.decorators import staff_required
 
 class AutoListView(View):
+    """
+    Vista para listar todos los autos.
+
+    Esta vista recupera todos los objetos de auto utilizando el repositorio AutoRepository 
+    y los renderiza en una plantilla de lista de autos.
+    """
     def get(self, request, *args, **kwargs):
         repo = AutoRepository()
         autos = repo.get_all()
         return render(request, 'autos/list.html', dict(autos=autos))
 
 class AutoDetailView(View):
+    """
+    Vista para mostrar los detalles de un auto específico.
+
+    Recupera un auto por su ID y muestra sus detalles junto con los comentarios asociados.
+    """
     def get(self, request, id, *args, **kwargs):
         repo = AutoRepository()
         auto = get_object_or_404(Auto, id=id)
@@ -23,6 +34,12 @@ class AutoDetailView(View):
 
 @method_decorator([login_required, staff_required], name='dispatch')
 class AutoDeleteView(View):
+    """
+    Vista para eliminar un auto.
+
+    Requiere que el usuario esté autenticado y tenga permisos de personal (staff).
+    Solo permite peticiones POST para eliminar el auto.
+    """
     def post(self, request, id, *args, **kwargs):
         repo = AutoRepository()
         auto = get_object_or_404(Auto, id=id)
@@ -30,17 +47,39 @@ class AutoDeleteView(View):
         return redirect('auto-list')
 
     def get(self, request, id, *args, **kwargs):
+        """
+        Método para manejar peticiones GET no permitidas.
+
+        Retorna un HTTP 405 (Method Not Allowed) si se intenta acceder a la vista con un método GET.
+        """
         return HttpResponseNotAllowed(['POST'])
 
 @method_decorator([login_required, staff_required], name='dispatch')
 class AutoUpdateView(View):
+    """
+    Vista para actualizar la información de un auto existente.
+
+    Proporciona un formulario para editar los datos del auto y guarda los cambios
+    si el formulario es válido.
+    """
     def get(self, request, id, *args, **kwargs):
+        """
+        Maneja las peticiones GET para mostrar el formulario de actualización del auto.
+
+        Renderiza la plantilla de actualización del auto con el formulario pre-llenado.
+        """
         repo = AutoRepository()
         auto = get_object_or_404(Auto, id=id)
         form = AutoForm(instance=auto)
         return render(request, 'autos/update.html', {'form': form, 'auto': auto})
 
     def post(self, request, id, *args, **kwargs):
+        """
+        Maneja las peticiones POST para procesar el formulario de actualización del auto.
+
+        Verifica la validez del formulario y guarda los cambios, redirigiendo a la vista de detalles
+        del auto si tiene éxito.
+        """
         repo = AutoRepository()
         auto = get_object_or_404(Auto, id=id)
         form = AutoForm(request.POST, request.FILES, instance=auto)
@@ -51,16 +90,33 @@ class AutoUpdateView(View):
 
 @method_decorator([login_required, staff_required], name='dispatch')
 class AutoCreateView(View):
+    """
+    Vista para crear un nuevo auto.
+
+    Permite a los usuarios autenticados y con permisos de personal (staff) crear un nuevo auto
+    mediante un formulario.
+    """
     def get(self, request, *args, **kwargs):
+        """
+        Maneja las peticiones GET para mostrar el formulario de creación de autos.
+
+        Renderiza la plantilla de creación de autos con el formulario vacío.
+        """
         form = AutoForm()
         return render(request, 'autos/create.html', {'form': form})
 
     def post(self, request, *args, **kwargs):
+        """
+        Maneja las peticiones POST para procesar el formulario de creación de autos.
+
+        Verifica la validez del formulario y guarda el nuevo auto, redirigiendo a la lista de autos si tiene éxito.
+        """
         form = AutoForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('auto-list')
         else:
+            # Ajusta el queryset del campo 'modelo' si el campo 'marca' ha cambiado
             if 'marca' in request.POST:
                 try:
                     marca_id = int(request.POST.get('marca'))
--- home/views.py
+++ home/views.py
@@ -6,12 +6,30 @@ from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class AuthView(View):
+    """
+    Vista para manejar el registro e inicio de sesión de usuarios.
+
+    Esta vista gestiona tanto el registro como el inicio de sesión de usuarios, utilizando
+    un único formulario que cambia su comportamiento según los datos recibidos.
+    """
     def get(self, request):
+        """
+        Maneja las peticiones GET para mostrar los formularios de login y registro.
+
+        Renderiza la plantilla de autenticación con los formularios de inicio de sesión y registro.
+        """
         login_form = UserLoginForm()
         register_form = UserRegistrationForm()
         return render(request, 'home/auth.html', {'login_form': login_form, 'register_form': register_form})
 
     def post(self, request):
+        """
+        Maneja las peticiones POST para procesar el registro o inicio de sesión.
+
+        - Procesa el registro de un nuevo usuario si se reciben los campos de registro.
+        - Procesa el inicio de sesión de un usuario existente si se reciben los campos de inicio de sesión.
+        """
+        # Procesar registro de usuario
         if 'username' in request.POST and 'password1' in request.POST and 'password2' in request.POST:
             register_form = UserRegistrationForm(request.POST)
             if register_form.is_valid():
@@ -24,6 +42,7 @@ class AuthView(View):
                     return redirect('index')
             else:
                 return render(request, 'home/auth.html', {'register_form': register_form, 'login_form': UserLoginForm()})
+        # Procesar inicio de sesión de usuario
         elif 'username' in request.POST and 'password' in request.POST:
             login_form = UserLoginForm(data=request.POST)
             if login_form.is_valid():
@@ -33,18 +52,44 @@ class AuthView(View):
                     return redirect('index')
             return render(request, 'home/auth.html', {'login_form': login_form, 'register_form': UserRegistrationForm()})
         return redirect('login')
+
 class LogoutView(View):
+    """
+    Vista para manejar el cierre de sesión de los usuarios.
+
+    Esta vista simplemente cierra la sesión del usuario actual y redirige a la página de inicio de sesión.
+    """
     def get(self, request):
+        """
+        Maneja las peticiones GET para cerrar la sesión del usuario.
+
+        Redirige al usuario a la página de inicio de sesión después de cerrar sesión.
+        """
         logout(request)
         return redirect('login')
 
 
 class RegisterView(View):
+    """
+    Vista para manejar el registro de nuevos usuarios.
+
+    Proporciona un formulario para que los nuevos usuarios se registren en el sistema.
+    """
     def get(self, request):
+        """
+        Maneja las peticiones GET para mostrar el formulario de registro.
+
+        Renderiza la plantilla de registro con el formulario correspondiente.
+        """
         form = UserRegistrationForm()
         return render(request, 'home/register.html', {'form': form})
 
     def post(self, request):
+        """
+        Maneja las peticiones POST para procesar el formulario de registro.
+
+        Crea un nuevo usuario si el formulario es válido y lo autentica automáticamente.
+        """
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             new_user = form.save(commit=False)
@@ -57,10 +102,20 @@ class RegisterView(View):
         return render(request, 'home/register.html', {'form': form})
 
 class IndexView(LoginRequiredMixin, TemplateView):
+    """
+    Vista para la página de inicio principal del sistema.
+
+    Esta vista utiliza un TemplateView genérico y requiere que el usuario esté autenticado para acceder.
+    """
     template_name = 'home/index.html'
     login_url = 'login'
 
     def get_context_data(self, **kwargs):
+        """
+        Proporciona el contexto adicional para la plantilla de inicio.
+
+        Añade el usuario autenticado al contexto de la plantilla.
+        """
         context = super().get_context_data(**kwargs)
         context['user'] = self.request.user
         return context
