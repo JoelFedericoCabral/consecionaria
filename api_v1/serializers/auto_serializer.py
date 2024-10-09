b9fce04b6354f4bdf1fb65c0849d9fdec3250d6c
Joel Cabral <108500847+JoelFedericoCabral@users.noreply.github.com>
2024-10-08 22:51:41 -0300

    add CategorySerializer

--- api_v1/serializers/auto_serializer.py
+++ api_v1/serializers/auto_serializer.py
@@ -1,10 +1,38 @@
 from rest_framework import serializers
 
-from gestion.models import Auto
+from gestion.models import Auto, Categoria
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
 
 
 class AutoSerializer(serializers.ModelSerializer):
+    categoria = CategorySerializer()
+    description = serializers.SerializerMethodField()
+
     class Meta:
         model = Auto
         fields = '__all__'
 
+    def get_description(self, value):
+        if value.descripcion is None:
+            return "No posee descripcion"
+        return value.descripcion
+    
+    def update(self, instance, validated_data):
+        category_data = validated_data.pop(
+            'categoria', None
+        )
+
+        categoria, _= Categoria.object.get_or_create(
+            **category_data
+        )
+
+        instance.categoria = categoria
+        instance.nombre = validated_data.get('nombre', instance.nombre)
+        instance.save()
+
+        return instance
--- gestion/models.py
+++ gestion/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
 class Marca(models.Model):
     """
     Modelo que representa una marca de autos.
@@ -13,35 +15,23 @@ class Marca(models.Model):
         """
         return self.nombre
 
+
 class ModeloAuto(models.Model):
-    """
-    Modelo que representa un modelo específico de auto, asociado a una marca.
-    """
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100)
 
     def __str__(self):
-        """
-        Devuelve la representación en cadena del modelo, que es el nombre del modelo.
-        """
         return f'{self.nombre}'
 
+
 class Categoria(models.Model):
-    """
-    Modelo que representa una categoría de autos.
-    """
     nombre = models.CharField(max_length=100)
 
     def __str__(self):
-        """
-        Devuelve la representación en cadena del modelo, que es el nombre de la categoría.
-        """
         return self.nombre
 
+
 class Auto(models.Model):
-    """
-    Modelo que representa un auto específico con detalles como marca, modelo, categoría, precio, imagen y descripción.
-    """
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE, default=1)
     modelo = models.ForeignKey(ModeloAuto, on_delete=models.CASCADE)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
@@ -50,54 +40,37 @@ class Auto(models.Model):
     descripcion = models.TextField()
 
     def __str__(self):
-        """
-        Devuelve la representación en cadena del modelo, que es la marca y el modelo del auto.
-        """
         return f'{self.modelo.marca} {self.modelo.nombre}'
 
+
 class Cliente(models.Model):
-    """
-    Modelo que representa un cliente, que está relacionado uno a uno con un usuario del sistema.
-    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     telefono = models.CharField(max_length=15)
     direccion = models.CharField(max_length=255)
 
     def __str__(self):
-        """
-        Devuelve la representación en cadena del modelo, que es el nombre de usuario del cliente.
-        """
         return self.user.username
 
+
 class Comentario(models.Model):
-    """
-    Modelo que representa un comentario hecho por un usuario sobre un auto.
-    """
     auto = models.ForeignKey(Auto, on_delete=models.CASCADE)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     comentario = models.TextField()
     fecha = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """
-        Devuelve la representación en cadena del modelo, que incluye el nombre de usuario del autor y el nombre del modelo del auto.
-        """
         return f'{self.autor.username} - {self.auto.modelo.nombre}'
 
+
 class Venta(models.Model):
-    """
-    Modelo que representa una venta de un auto a un cliente, con detalles de fecha y precio final.
-    """
     auto = models.ForeignKey(Auto, on_delete=models.CASCADE)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     fecha = models.DateTimeField(auto_now_add=True)
     precio_final = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        """
-        Devuelve la representación en cadena del modelo, que incluye el nombre de usuario del cliente y el nombre del modelo del auto vendido.
-        """
         return f'{self.cliente.user.username} - {self.auto.modelo.nombre}'
+
 
 class Empleado(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -113,67 +86,44 @@ class Empleado(models.Model):
 
 
 class Proveedor(models.Model):
-    """
-    Modelo que representa un proveedor que suministra autos a la concesionaria.
-    """
     nombre = models.CharField(max_length=100)
     telefono = models.CharField(max_length=15)
     direccion = models.CharField(max_length=255)
     autos = models.ManyToManyField(Auto)
 
     def __str__(self):
-        """
-        Devuelve la representación en cadena del modelo, que es el nombre del proveedor.
-        """
         return self.nombre
 
+
 class Servicio(models.Model):
-    """
-    Modelo que representa un servicio ofrecido por la concesionaria.
-    """
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        """
-        Devuelve la representación en cadena del modelo, que es el nombre del servicio.
-        """
         return self.nombre
 
+
 class Cita(models.Model):
-    """
-    Modelo que representa una cita de un cliente para un servicio específico.
-    """
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
     fecha = models.DateTimeField()
     descripcion = models.TextField()
 
     def __str__(self):
-        """
-        Devuelve la representación en cadena del modelo, que incluye el nombre de usuario del cliente y el nombre del servicio.
-        """
         return f'{self.cliente.user.username} - {self.servicio.nombre}'
 
+
 class Inventario(models.Model):
-    """
-    Modelo que representa el inventario de autos en la concesionaria, con la cantidad disponible y fecha de actualización.
-    """
     auto = models.ForeignKey(Auto, on_delete=models.CASCADE)
     cantidad = models.PositiveIntegerField()
     fecha_actualizacion = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        """
-        Devuelve la representación en cadena del modelo, que incluye el nombre del modelo del auto y la cantidad disponible.
-        """
         return f'Inventario {self.auto.modelo.nombre} - {self.cantidad}'
 
+
 class Financiamiento(models.Model):
-    """
-    Modelo que representa un financiamiento otorgado a un cliente para la compra de un auto.
-    """
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     auto = models.ForeignKey(Auto, on_delete=models.CASCADE)
     monto_financiado = models.DecimalField(max_digits=10, decimal_places=2)
@@ -183,7 +133,4 @@ class Financiamiento(models.Model):
     fecha_fin = models.DateTimeField()
 
     def __str__(self):
-        """
-        Devuelve la representación en cadena del modelo, que incluye el nombre de usuario del cliente y el nombre del modelo del auto.
-        """
         return f'Financiamiento {self.cliente.user.username} - {self.auto.modelo.nombre}'
